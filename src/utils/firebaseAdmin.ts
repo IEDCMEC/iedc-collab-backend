b8ea0847a8acd9d92607fecb16ea87d8ba828715
Jaison Dennis <jaisondennis080@gmail.com>
2023-12-26 12:41:46 +0530

    Fixed Issue of JSON Parsing

--- src/utils/firebaseAdmin.ts
+++ src/utils/firebaseAdmin.ts
@@ -1,6 +1,7 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = atob(process.env.SERVICE_ACCOUNT_ENCODED!)
+var serviceAccount = JSON.parse(atob(process.env.SERVICE_ACCOUNT_ENCODED!));
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
