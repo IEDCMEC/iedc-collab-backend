0c9c0622b733824a6453c253ea05fcabe546e8c8
Jaison Dennis <jaisondennis080@gmail.com>
2023-12-26 15:20:24 +0530

    Cors fro Middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -6,9 +6,15 @@
 // });
 
 import admin from "@/utils/firebaseAdmin";
+import NextCors from "nextjs-cors";
 
 export function withAuth(handler: any) {
   return async (req: any, res: any) => {
+    await NextCors(req, res, {
+      methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+      origin: "*",
+      optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
     try {
       const token = req.headers["x-auth-token"];
       if (!token) {
